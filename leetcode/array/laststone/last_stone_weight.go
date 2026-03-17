b8a36a02cb007c53bc8dbaac0e00aa04070390ae
chaewonkong <chaewonkong@gmail.com>
2026-03-17 22:34:18 +0900

    feat: last stone weight

---
+++ leetcode/array/laststone/last_stone_weight.go
@@ -0,0 +1,87 @@
+package laststone
+
+func lastStoneWeight(stones []int) int {
+	h := newHeap()
+	for _, stone := range stones {
+		h.insert(stone)
+	}
+
+	var last int
+	for {
+		y, ok := h.popMax()
+		if !ok {
+			break // has next
+		}
+		x, ok := h.popMax()
+		if !ok {
+			last = y
+			break
+		}
+
+		if x == y {
+			continue
+		}
+		h.insert(y - x)
+	}
+
+	return last
+}
+
+type heap struct {
+	stones []int
+}
+
+func newHeap() *heap {
+	return &heap{
+		stones: []int{0},
+	}
+}
+
+func (h *heap) insert(v int) {
+	last := len(h.stones)
+	h.stones = append(h.stones, v)
+
+	// 아래에서부터 비교하며 교환해 올라가기
+	cur := last
+	parent := cur / 2 // binary tree
+
+	for parent >= 1 && h.stones[parent] < h.stones[cur] {
+		h.stones[parent], h.stones[cur] = h.stones[cur], h.stones[parent]
+		cur = parent
+		parent = parent / 2
+	}
+}
+
+func (h *heap) popMax() (int, bool) {
+	if len(h.stones) == 1 {
+		return 0, false
+	}
+
+	// pop root, move last to root pos, compare items starting from root
+	root := h.stones[1]
+	last := len(h.stones) - 1
+	h.stones[1] = h.stones[last]
+	h.stones = h.stones[:last]
+
+	last = last - 1
+	i := 1
+	for i <= last {
+		swap := i
+
+		if idx := 2 * i; idx <= last && h.stones[idx] > h.stones[swap] {
+			swap = idx
+		}
+		if idx := 2*i + 1; idx <= last && h.stones[idx] > h.stones[swap] {
+			swap = idx
+		}
+
+		if swap != i {
+			h.stones[swap], h.stones[i] = h.stones[i], h.stones[swap]
+			i = swap
+		} else {
+			break
+		}
+	}
+
+	return root, true
+}
