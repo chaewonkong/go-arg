1c40fb004f7edd8c164733e134ffc62589eea829
chaewonkong <chaewonkong@gmail.com>
2026-04-01 23:16:02 +0900

    feat: insert inverval

---
+++ leetcode/interval/insertinterval/insert_interval.go
@@ -0,0 +1,25 @@
+package insertinterval
+
+func insert(intervals [][]int, newInterval []int) [][]int {
+	result := make([][]int, 0, len(intervals)+1)
+
+	i := 0
+	for i < len(intervals) && intervals[i][1] < newInterval[0] {
+		result = append(result, intervals[i])
+		i++
+	}
+
+	for i < len(intervals) && intervals[i][0] <= newInterval[1] {
+		newInterval[0] = min(intervals[i][0], newInterval[0])
+		newInterval[1] = max(intervals[i][1], newInterval[1])
+		i++
+	}
+	result = append(result, newInterval)
+
+	for i < len(intervals) {
+		result = append(result, intervals[i])
+		i++
+	}
+
+	return result
+}
