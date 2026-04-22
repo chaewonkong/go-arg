be8d8f35d92b65c810019fbd2f57c1f0d34e0aa3
chaewonkong <chaewonkong@gmail.com>
2026-04-23 00:00:34 +0900

    feat: redundant connection with union find

---
+++ leetcode/unionfind/redundantconn/redundant_conn.go
@@ -0,0 +1,46 @@
+// Package redundantconn
+//
+//   - https://leetcode.com/problems/redundant-connection/description/
+package redundantconn
+
+/* Union Find
+for each edge (u, v):
+  if find(u) == find(v): // 이미 연결됨. 정답.
+  else:
+    union(u, v)
+* */
+
+func findRedundantConnection(edges [][]int) []int {
+	n := len(edges)
+	parent := make([]int, n+1)
+	for i := range parent {
+		parent[i] = i
+	}
+
+	var find func(x int) int
+	find = func(x int) int {
+		if parent[x] != x {
+			parent[x] = find(parent[x])
+		}
+		return parent[x]
+	}
+
+	union := func(x, y int) bool {
+		px, py := find(x), find(y)
+		if px == py {
+			// same root -> circular
+			return false
+		}
+
+		parent[py] = px
+		return true
+	}
+
+	for _, edge := range edges {
+		if !union(edge[0], edge[1]) {
+			return edge
+		}
+	}
+
+	return nil
+}
